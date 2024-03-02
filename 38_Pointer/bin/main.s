e4646636336562e44b830a8138c6ace8e1a633d8
aliasghor <aliasghor6@gmail.com>
2024-03-02 23:55:35 +0800

    Commiting my folder part 28 and 38

---
+++ 38_Pointer/bin/main.s
@@ -0,0 +1,229 @@
+	.file	"main.cpp"
+	.text
+	.section	.text$_ZN5HumanC1EPKcRKi,"x"
+	.linkonce discard
+	.align 2
+	.globl	_ZN5HumanC1EPKcRKi
+	.def	_ZN5HumanC1EPKcRKi;	.scl	2;	.type	32;	.endef
+	.seh_proc	_ZN5HumanC1EPKcRKi
+_ZN5HumanC1EPKcRKi:
+.LFB2226:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movq	%rdx, 24(%rbp)
+	movq	%r8, 32(%rbp)
+	movq	16(%rbp), %rax
+	movq	24(%rbp), %rdx
+	movq	%rdx, (%rax)
+	movq	32(%rbp), %rax
+	movl	(%rax), %edx
+	movq	16(%rbp), %rax
+	movl	%edx, 8(%rax)
+	nop
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Ali Ganteng\0"
+.LC1:
+	.ascii "Human's name = \0"
+.LC2:
+	.ascii "Human's age = \0"
+.LC3:
+	.ascii "num_int value = \0"
+.LC4:
+	.ascii "num_int memory address = \0"
+.LC5:
+	.ascii "intPtr value = \0"
+.LC6:
+	.ascii "intPtr memory address = \0"
+	.align 8
+.LC7:
+	.ascii "intPtr value after the num_int value has been changed = \0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB2227:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$72, %rsp
+	.seh_stackalloc	72
+	leaq	64(%rsp), %rbp
+	.seh_setframe	%rbp, 64
+	.seh_endprologue
+	call	__main
+	movl	$16, %ecx
+	call	_Znwy
+	movq	%rax, %rbx
+	movl	$21, -20(%rbp)
+	leaq	-20(%rbp), %rax
+	movq	%rax, %r8
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	movq	%rbx, %rcx
+	call	_ZN5HumanC1EPKcRKi
+	movq	%rbx, -8(%rbp)
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	-8(%rbp), %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	movl	$10, %edx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	movl	$5, -24(%rbp)
+	leaq	-24(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-24(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPKv
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	movl	$10, %edx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	leaq	.LC5(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	-16(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	leaq	.LC6(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPKv
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	movl	$100, -24(%rbp)
+	leaq	.LC7(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	-16(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	movq	-8(%rbp), %rax
+	testq	%rax, %rax
+	je	.L3
+	movl	$16, %edx
+	movq	%rax, %rcx
+	call	_ZdlPvy
+.L3:
+	movq	.refptr._ZSt3cin(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSi3getEv
+	movl	$0, %eax
+	addq	$72, %rsp
+	popq	%rbx
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.ident	"GCC: (Rev3, Built by MSYS2 project) 13.2.0"
+	.def	_Znwy;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEi;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEPKv;	.scl	2;	.type	32;	.endef
+	.def	_ZdlPvy;	.scl	2;	.type	32;	.endef
+	.def	_ZNSi3getEv;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt3cin, "dr"
+	.globl	.refptr._ZSt3cin
+	.linkonce	discard
+.refptr._ZSt3cin:
+	.quad	_ZSt3cin
+	.section	.rdata$.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, "dr"
+	.globl	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	.linkonce	discard
+.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_:
+	.quad	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
